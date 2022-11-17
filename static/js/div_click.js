17b9c5c62816bf794b8128f1f3e18702e223b3b1
Nikita Filin <nikita10filin@gmail.com>
2022-11-17 23:18:46 +0200

    github pages setup

--- static/js/div_click.js
+++ static/js/div_click.js
@@ -8,7 +8,7 @@ function popup(element, html, popup) {
         el.onclick = function () {
             localStorage.setItem("blur", "yes")
             localStorage.setItem("popup", popup)
-            document.location.href = '../sport.htm';
+            document.location.href = '../sport.html';
         }
     }
 }
